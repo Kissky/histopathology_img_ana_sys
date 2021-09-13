8687bc6433ac14aaa564e7915618346093bfbb93
Kissky <DY-WangJiarong@alumni.sjtu.edu.cn>
2021-09-13 15:35:44 +0800

    Test1

--- learn/learn_calculator.py
+++ learn/learn_calculator.py
@@ -5,6 +5,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QCoreApplication
 
 
 class Example(QWidget):
@@ -13,16 +14,22 @@ class Example(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        QToolTip.setFont(QFont('SansSerif', 10))
-        self.setToolTip('This is a <b>QWidget</b> widget')
+        QToolTip.setFont(QFont('华文行楷', 20))
+        self.setToolTip('This is a <b><i>QWidget</i></b> widget')
 
         btn = QPushButton('Button', self)
         btn.setToolTip('This is a <b>QPushButton</b> widget')
         btn.resize(btn.sizeHint())
         btn.move(50, 50)
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
 
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit())
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(100, 300)
+
+        self.setGeometry(1000, 300, 1000, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('C:/Document/NXU/HTML/NXUHelper/Pics/NXU.png'))
         self.show()
 
 
@@ -30,9 +37,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     ex = Example()
-    w = QWidget()
-    w.resize(1000, 500)
-    w.move(1500, 300)
-    w.setWindowTitle('Simple')
-    w.show()
+    # w = QWidget()
+    # w.resize(1000, 500)
+    # w.move(1500, 300)
+    # w.setWindowTitle('Simple')
+    # w.show()
     sys.exit(app.exec_())
