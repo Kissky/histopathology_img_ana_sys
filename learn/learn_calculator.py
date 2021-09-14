aca4de85c83935a560f809f109d5071f9e1f12cd
Kissky <DY-WangJiarong@alumni.sjtu.edu.cn>
2021-09-14 10:38:57 +0800

    Test1

--- learn/learn_calculator.py
+++ learn/learn_calculator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox, QDesktopWidget
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QCoreApplication
@@ -22,20 +22,34 @@ class Example(QWidget):
         btn.resize(btn.sizeHint())
         btn.move(50, 50)
 
-        qbtn = QPushButton('Quit', self)
-        qbtn.clicked.connect(QCoreApplication.instance().quit())
-        qbtn.resize(qbtn.sizeHint())
-        qbtn.move(100, 300)
+        q_btn = QPushButton('Quit', self)
+        q_btn.clicked.connect(QCoreApplication.instance().quit)
+        q_btn.resize(q_btn.sizeHint())
+        q_btn.move(300, 100)
 
         self.setGeometry(1000, 300, 1000, 220)
+        self.center()
         self.setWindowTitle('Icon')
         self.setWindowIcon(QIcon('C:/Document/NXU/HTML/NXUHelper/Pics/NXU.png'))
         self.show()
 
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?', QMessageBox.Yes | QMessageBox.No,
+                                     QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     ex = Example()
     # w = QWidget()
     # w.resize(1000, 500)
